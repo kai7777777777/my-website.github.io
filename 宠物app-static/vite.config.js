7c1a2b9a17cdbbff9c89100495771827c4f9feb6
kai7777777777 <kai777734@gmail.com>
2025-05-12 15:48:49 +0800

    Upload static website files for GitHub Pages
    Added index.html, CSS, JS, and other static files for the pet app website.

---
+++ 宠物app-static/vite.config.js
@@ -0,0 +1,11 @@
+export default {
+  server: {
+    host: '0.0.0.0', // 允许外部设备访问
+    port: 5173, // 确保端口号正确
+  },
+  build: {
+    // rollupOptions: {
+    //   input: 'public/index.html',
+    // },
+  },
+}
